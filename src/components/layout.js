157990dc710678f0c1ba9c8e5f16ed80f8efe1b6
Allan <ariosmaia@yahoo.com.br>
2020-05-25 17:43:40 -0300

    Imagens com Gatsby

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import Profile from './Profile/index';
+import Profile from './Profile';
 
 const Layout = ({ children }) => {
 	return (
