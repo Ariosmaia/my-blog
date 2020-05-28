1ded943af8f72d5bb503c3dcc4b222cb4685075b
Allan <ariosmaia@yahoo.com.br>
2020-05-28 18:43:12 -0300

    Estilos do post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,14 +4,25 @@ import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
 
+import * as S from '../components/Post/styled';
+
 const BlogPost = ({ data }) => {
   const post = data.markdownRemark;
 
   return (
-    <>
-      <h1>Title: {post.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
-    </>
+    <Layout>
+      <SEO title={post.frontmatter.title} />
+      <S.PostHeader>
+        <S.PostDate>
+          {post.frontmatter.date} • {post.timeToRead} min de leitura
+        </S.PostDate>
+        <S.PostTitle>post.frontmatter.title</S.PostTitle>
+        <S.PostDescription>{post.frontmatter.description}</S.PostDescription>
+      </S.PostHeader>
+      <S.MainContent>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </S.MainContent>
+    </Layout>
   );
 };
 
@@ -20,8 +31,11 @@ export const query = graphql`
     markdownRemark(fields: { slug: { eq: $slug } }) {
       frontmatter {
         title
+        description
+        date(locale: "pt-br", formatString: "DDD [de] MMMM [de] YYYY")
       }
       html
+      timeToRead
     }
   }
 `;
