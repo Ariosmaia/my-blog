af67ad7d070c4256c8cbcbbe9deca93e98321468
Allan <ariosmaia@yahoo.com.br>
2020-05-30 18:23:49 -0300

    Criandocontexto de post anterior e proximo post

--- gatsby-node.js
+++ gatsby-node.js
@@ -39,18 +39,38 @@ exports.createPages = ({ graphql, actions }) => {
             }
             timeToRead
           }
+          next {
+            id
+            frontmatter {
+              title
+            }
+            fields {
+              slug
+            }
+          }
+          previous {
+            id
+            frontmatter {
+              title
+            }
+            fields {
+              slug
+            }
+          }
         }
       }
     }
   `).then((result) => {
     const posts = result.data.allMarkdownRemark.edges;
 
-    posts.forEach(({ node }) => {
+    posts.forEach(({ node, next, previous }) => {
       createPage({
         path: node.fields.slug,
         component: path.resolve('./src/templates/blog-post.js'),
         context: {
           slug: node.fields.slug,
+          previousPost: next,
+          nextPost: previous,
         },
       });
     });
